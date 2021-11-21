6747ab4ee4ecbc89649d07d4e57d0185d100977c
CoderCXF <18251716983@163.com>
2021-11-21 08:32:57 +0000

    commit

--- utils/parse_config.py
+++ utils/parse_config.py
@@ -1,25 +1,25 @@
-def parse_model_cfg(path):  # 解析YOLOv3——1088...这些配置文件
+def parse_model_cfg(path):
     """Parses the yolo-v3 layer configuration file and returns module definitions"""
     file = open(path, 'r')
     lines = file.read().split('\n')
     lines = [x for x in lines if x and not x.startswith('#')]
-    lines = [x.rstrip().lstrip() for x in lines]  # get rid of fringe whitespaces（去掉每一行的空白）
+    lines = [x.rstrip().lstrip() for x in lines]  # get rid of fringe whitespaces
     module_defs = []
     for line in lines:
-        if line.startswith('['):  # This marks the start of a new block，[代表着新块的开始
-            module_defs.append({})  # 如果是一个新块的话，就引入一个新的字典
-            module_defs[-1]['type'] = line[1:-1].rstrip()  # 读取网络块的类型：卷积层、或者等等其它层
+        if line.startswith('['):  # This marks the start of a new block
+            module_defs.append({})
+            module_defs[-1]['type'] = line[1:-1].rstrip()
             if module_defs[-1]['type'] == 'convolutional':
                 module_defs[-1]['batch_normalize'] = 0
         else:
-            key, value = line.split("=")   # 如果不是一个新块的话，用键值对进行保存。还是放在 moudle_defs这个字典中。
-                                           # 遍历循环之后，所有的yolov3_1088...的信息都保存在了moudle_defs这个字典中
+            key, value = line.split("=")
             value = value.strip()
             if value[0] == '$':
                 value = module_defs[0].get(value.strip('$'), None)
             module_defs[-1][key.rstrip()] = value
-    # 函数返回的是一个列表，保存所有的层块
+
     return module_defs
+
 
 def parse_data_cfg(path):
     """Parses the data configuration file"""
